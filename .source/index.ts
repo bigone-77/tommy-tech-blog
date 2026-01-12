f08ab3aab81c3f4bf0f2f8b84ece6aa6943fdbc8
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2026-01-13 08:09:35 +0900

    🔒️ [gitignore] 빌드 시 불필요하게 올라가는 파일들 ignore 추가

--- .source/index.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_1 from "../blog/content/707ddc1d-5c4c-428d-afb6-c39b087053f5.mdx?collection=docs&hash=1768258427938"
-import * as docs_0 from "../blog/content/6cd98c3b-550c-44f0-9ec7-dbdd3506e4ff.mdx?collection=docs&hash=1768258427938"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"6cd98c3b-550c-44f0-9ec7-dbdd3506e4ff.mdx","absolutePath":"/Users/shintaeil/Developer/tommy-tech-blog/blog/content/6cd98c3b-550c-44f0-9ec7-dbdd3506e4ff.mdx"}, data: docs_0 }, { info: {"path":"707ddc1d-5c4c-428d-afb6-c39b087053f5.mdx","absolutePath":"/Users/shintaeil/Developer/tommy-tech-blog/blog/content/707ddc1d-5c4c-428d-afb6-c39b087053f5.mdx"}, data: docs_1 }]);
-export const meta = _runtime.meta<typeof _source.meta>([]);
